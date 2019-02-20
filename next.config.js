79201d128adcbe5239f118e703876f0cd47470a0
Fernando Moreira <nandomoreira.me@gmail.com>
2019-02-20 19:33:33 -0300

    config webpack

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+'use strict'
+
 const withStylus = require('@zeit/next-stylus')
 
 module.exports = withStylus({
-  cssModules: true
+  cssModules: true,
+  webpack (cfg) {
+    cfg.plugins = cfg.plugins.filter(
+      plugin => plugin.constructor.name !== 'UglifyJsPlugin'
+    )
+
+    return cfg
+  }
 })
