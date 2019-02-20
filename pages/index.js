581838a021f09af5b8933f1e8d5521c5a6dd0fae
Fernando Moreira <nandomoreira.me@gmail.com>
2019-02-20 19:33:42 -0300

    Head

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
-import './global.styl'
+import Head from 'next/head'
 import Hero from '../components/Hero'
+
+import './global.styl'
 import styles from './Home.styl'
 
 function Home () {
   return (
     <Hero>
+      <Head>
+        <title>Welcome to next.js!</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h1 className={styles.h1}>
         Welcome to <a href="https://nextjs.org" target="_blank">next.js</a>!
       </h1>
